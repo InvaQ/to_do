70fa4f11527b386a88f18a8dc668a3346f2dea13
InvaQ <freerest02@gmail.com>
2016-08-10 22:11:25 +0300

    finished with styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  #post 'tasks/change_priority', to: 'tasks#change_priority', as: 'task'
   root 'lists#index'
   resources :lists
   resources :tasks do   
